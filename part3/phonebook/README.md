9cfbea659a60d7b1cd892912d759e4761868a489
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-09-20 12:45:50 +1000

    delete and post request initial task added

---
+++ part3/phonebook/README.md
@@ -0,0 +1,26 @@
+# Exercises 3..2 -3.6
+
+### 3.1 Phonebook backend
+
+.In this exercise I have to implement the return of hardcoded list of phonebook entris at the URL `http://localhost:3001/api/persons` using Node express.
+
+### 3.2 Phonebook backend
+
+.Expending the above exercise to implement the return summary of information "Phonebook has info for 4 people" and time of processing the request in the URL `http://localhost:3001/api/info`.
+
+### 3.3 Phonebook backend
+
+.Display information for a single phonebook entry exp.` URL http://localhost:3001/api/persons/5`.
+
+.Delete data using id parameter from URL
+.To test the functionality if the data been delete run "send request" on the URL
+`Get http://localhost:3001/api/persons/3`.
+.Folder name 'request' is implemented to run REST
+
+### part3_nodejs-5
+
+.Add new note.
+.Auto generate header content-type: with the help of json-parser / app.use(express.json()).
+.Retrive data from body property of the request object.
+
+.Create hard code data object to add new note.
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+//  jason-parser to access data to dd new notes in the request body in JSON format.
+app.use(express.json());
 
 let persons = [
   {
@@ -28,6 +30,73 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+// fetch all the data for summary infomation
+app.get("/info", (request, response) => {
+  const datalength = persons.length;
+  const datetime = new Date();
+  response.send(
+    `<h5>Phonebook has info for ${datalength} people</h5>
+    <h6>${datetime}</h6>`
+  );
+});
+
+// Display single phonebook entry information and if not found set and error status code.
+
+// check for individual id to load from the url to code to filter
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// deleting data
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((perperson) => perperson.id !== id);
+  console.log(persons);
+  response.status(204).end();
+});
+
+// add new person
+
+const generateId = () => Math.floor(Math.random() * 100);
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  let autoid = generateId();
+  let result = preventDoubleid(autoid);
+
+  if (result) {
+    window.confirm(
+      `${result.name} is already added to phonebook. Do you want to change the number?`
+    );
+  }
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: autoid,
+  };
+
+  persons = persons.concat(person);
+  console.log(persons.id);
+  response.json(persons);
+});
+
+// prevent double entry
+const preventDoubleid = (sameid) =>
+  persons.find(({ id }) => id.includes(sameid));
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
