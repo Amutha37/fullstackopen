9731df4b333330b2b38bcf775143ff3baead8819
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-01 15:52:51 +1100

    remove console.log

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,49 +1,49 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from 'react'
 
-import PrintContacts from "./components/PrintContacts";
-import Notification from "./components/Notification";
-import SearchBar from "./components/SearchBar";
-import Personform from "./components/Personform";
-import "./App.css";
-import personsService from "./services/fetchpersons";
+import PrintContacts from './components/PrintContacts'
+import Notification from './components/Notification'
+import SearchBar from './components/SearchBar'
+import Personform from './components/Personform'
+import './App.css'
+import personsService from './services/fetchpersons'
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [searchName, setSearchName] = useState("");
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [filteredList, setfilteredList] = useState([]);
+  const [persons, setPersons] = useState([])
+  const [searchName, setSearchName] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [filteredList, setfilteredList] = useState([])
 
   //  fetch data from json server
   useEffect(() => {
     personsService.getAll().then((initialPersons) => {
-      setPersons(initialPersons);
-    });
-  }, []);
+      setPersons(initialPersons)
+    })
+  }, [])
 
   // addcontact
   const addContact = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     if (!newName || !newNumber) {
-      alert(`Please enter contact name and number.`);
+      alert(`Please enter contact name and number.`)
     } else {
-      let result = preventDoubleName(newName);
+      let result = preventDoubleName(newName)
 
       if (result) {
         window.confirm(
           `${result.name} is already added to phonebook. Do you want to change the number?`
-        );
+        )
         {
           const updatePerson = persons.find(
             (n) => n.name.toLowerCase() === newName.toLowerCase()
-          );
+          )
           const changedNumber = {
             ...updatePerson,
             number: newNumber,
             date: new Date(),
-          };
+          }
 
           personsService
             .update(updatePerson.id, changedNumber)
@@ -52,57 +52,57 @@ const App = () => {
                 persons.map((pers) =>
                   pers.id !== updatePerson.id ? pers : returnedPerson
                 )
-              );
+              )
               setErrorMessage(
                 `${updatePerson.name}'s contact was successfully updated.`
-              );
+              )
               setTimeout(() => {
-                setErrorMessage(null);
-              }, 5000);
+                setErrorMessage(null)
+              }, 5000)
             })
             .catch((error) => {
               setErrorMessage(
                 `ERROR!  ${updatePerson.name} has already been removed from server.`
-              );
+              )
               setTimeout(() => {
-                setErrorMessage(null);
-              }, 5000);
-            });
+                setErrorMessage(null)
+              }, 5000)
+            })
         }
       } else {
-        let lastPosition = persons[persons.length - 1];
-        let lastId = lastPosition.id;
+        let lastPosition = persons[persons.length - 1]
+        let lastId = lastPosition.id
 
-        const charcap = characterCapital(newName);
+        const charcap = characterCapital(newName)
 
         const newname = {
           id: lastId + 1,
           name: charcap.trim(),
           date: new Date(),
           number: newNumber,
-        };
+        }
         personsService
           .create(newname)
           .then((returnedPersons) => {
-            setPersons(persons.concat(returnedPersons));
-            setErrorMessage(`Added '${returnedPersons.name}'`);
+            setPersons(persons.concat(returnedPersons))
+            setErrorMessage(`Added '${returnedPersons.name}'`)
             setTimeout(() => {
-              setErrorMessage(null);
-            }, 3000);
+              setErrorMessage(null)
+            }, 3000)
           })
           .catch((e) => {
             setErrorMessage(
               `Error : Minimum length for name is (3) and minimum length for phone number is (8)`
-            );
+            )
             setTimeout(() => {
-              setErrorMessage(null);
-            }, 5000);
-          });
+              setErrorMessage(null)
+            }, 5000)
+          })
       }
     }
-    setNewName("");
-    setNewNumber("");
-  };
+    setNewName('')
+    setNewNumber('')
+  }
   // First letter of the names set to capital letter
   const characterCapital = (str) => {
     return str
@@ -110,50 +110,50 @@ const App = () => {
       .map(
         (word) => `${word.substring(0, 1).toUpperCase()}${word.substring(1)}`
       )
-      .join(" ");
-  };
+      .join(' ')
+  }
 
   // handlenewname and number
-  const handleNewName = (event) => setNewName(event.target.value);
-  const handleNewNumber = (event) => setNewNumber(event.target.value);
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
 
   // Check if user entered the same name
 
   const preventDoubleName = (samename) =>
+    // eslint-disable-next-line array-callback-return
     persons.find(({ name }) => {
-      console.log(name.length, samename.length);
       if (name.length === samename.length) {
-        return name.toLowerCase().includes(samename.toLowerCase().trim());
+        return name.toLowerCase().includes(samename.toLowerCase().trim())
       }
-    });
+    })
   // SEARCH BAR TARGET VALUE
   const handlesearch = (event) => {
-    setSearchName(event.target.value);
+    setSearchName(event.target.value)
 
     const filtered = persons.filter((row) =>
       row.name.toLowerCase().includes(searchName.toLowerCase())
-    );
-    setfilteredList(filtered);
-  };
+    )
+    setfilteredList(filtered)
+  }
 
   // handle delete
 
   const handleDeleteContact = (event) => {
     // const btnValue = parseFloat(event.target.value);
-    const btnValue = event.target.value;
-    const deletePerson = persons.find((n) => n.id === btnValue);
+    const btnValue = event.target.value
+    const deletePerson = persons.find((n) => n.id === btnValue)
 
     if (
       window.confirm(`Do you want to delete ${deletePerson.name}'s contact?`)
     ) {
       if (searchName) {
-        setfilteredList(filteredList.filter((n) => n.id !== deletePerson.id));
+        setfilteredList(filteredList.filter((n) => n.id !== deletePerson.id))
       } else {
-        personsService.delContact(deletePerson.id);
-        setPersons(persons.filter((n) => n.id !== deletePerson.id));
+        personsService.delContact(deletePerson.id)
+        setPersons(persons.filter((n) => n.id !== deletePerson.id))
       }
     }
-  };
+  }
 
   return (
     <Fragment>
@@ -182,7 +182,7 @@ const App = () => {
         />
       )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default App;
+export default App
