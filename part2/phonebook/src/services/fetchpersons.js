86d8d085c7026ba866cd4939b7c54a05b7930ce1
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-02 16:16:58 +1100

    remove catch from update

--- part2/phonebook/src/services/fetchpersons.js
+++ part2/phonebook/src/services/fetchpersons.js
@@ -1,42 +1,42 @@
-import axios from "axios";
+import axios from 'axios'
 // JSON server dev
 // const baseUrl = "http://localhost:3001/persons/";
 // backend fetch from express
-// const baseUrl = "http://localhost:3001/api/persons";
+// const baseUrl = 'http://localhost:3001/api/persons'
 
 // backend heroku express
 // const baseUrl = `https://quiet-dawn-80146.herokuapp.com/api/persons`;
 
 // production site
 // The web domain url is same as the backend as for frontend for this we use the relative url.
-const baseUrl = `/api/persons`;
+const baseUrl = `/api/persons`
 
 // list all contact
 const getAll = () => {
-  const request = axios.get(baseUrl);
+  const request = axios.get(baseUrl)
 
-  return request.then((response) => response.data);
-};
+  return request.then((response) => response.data)
+}
 // add new contact
 const create = (newname) => {
-  const request = axios.post(baseUrl, newname);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseUrl, newname)
+  return request.then((response) => response.data)
+}
 // update
 
 const update = (iddPerson, changedNumber) => {
-  const request = axios.put(`${baseUrl}/${iddPerson}`, changedNumber);
-  return request.then((response) => response.data);
-};
+  const request = axios.put(`${baseUrl}/${iddPerson}`, changedNumber)
+  return request.then((response) => response.data)
+}
 // delete contact
 const delContact = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
-};
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
+}
 const fetchAll = {
   getAll,
   create,
   delContact,
   update,
-};
-export default fetchAll;
+}
+export default fetchAll
