6799630ddf484c21e5a1f3d9cc88d3b4740409dc
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-08 15:27:26 +1100

    add .eslintrc file

--- part4/blog_list/.eslintrc.js
+++ part4/blog_list/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
+    node: true,
+    jest: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 12,
   },
   rules: {
-    indent: ['error', 4],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
+    'no-console': 0,
   },
 }
