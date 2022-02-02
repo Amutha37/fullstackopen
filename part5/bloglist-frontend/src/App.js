63ebdca95fb5093b8ea2a18c76325736cc9c58f1
Amutha Muhunthan <tamamutha@gmail.com>
2022-02-02 12:58:12 +1100

    display blog list in a table

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [errTextColour, setErrTextColour] = useState(true)
   const [blogs, setBlogs] = useState([])
-
+  const [showing, setShowing] = useState(false)
   // == new blog list local state ===
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
@@ -36,28 +36,26 @@ const App = () => {
     event.preventDefault()
 
     const newBlog = { title, author, url }
-    // const newBlog = {
-    //   title: title,
-    //   author: author,
-    //   url: url,
-    // }
+
     setErrTextColour(false)
     try {
       const saveBlog = await blogService.create(newBlog)
-      // .then((returnedBlog) => {
-      // setBlogs(blogs.concat(returnedBlog))
+
       setBlogs([...blogs, saveBlog])
       setTitle('')
       setUrl('')
       setAuthor('')
+      setShowing(true)
       setErrorMessage(`Blog '${newBlog.title}' succesfully saved.`)
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
+
       // })
     } catch (error) {
       console.log(error.response.data)
       setErrTextColour(true)
+      setShowing(true)
       setErrorMessage(error.response.data)
       setTimeout(() => {
         setErrorMessage(null)
@@ -68,34 +66,33 @@ const App = () => {
   // === New Blog list form ===
   const blogForm = () => (
     <form onSubmit={addBlog} className='blog_list_container'>
-      {/* <label>
-    Name:
-    <input type="text" name="name" />
-  </label> */}
-      <label>Title : </label>
-      <input
-        type='text'
-        value={title}
-        name='title'
-        onChange={({ target }) => setTitle(target.value)}
-      />
-
-      <label>Author : </label>
-      <input
-        type='text'
-        value={author}
-        name='author'
-        onChange={({ target }) => setAuthor(target.value)}
-      />
-
-      <label>URL : </label>
-      <input
-        type='text'
-        value={url}
-        name='url'
-        onChange={({ target }) => setUrl(target.value)}
-      />
-
+      <label>
+        Title :
+        <input
+          type='text'
+          value={title}
+          name='title'
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </label>
+      <label>
+        Author :
+        <input
+          type='text'
+          value={author}
+          name='author'
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </label>
+      <label>
+        URL :
+        <input
+          type='text'
+          value={url}
+          name='url'
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </label>
       <button type='submit'>Save</button>
     </form>
   )
@@ -115,6 +112,7 @@ const App = () => {
       setPassword('')
     } catch (exception) {
       setErrTextColour(true)
+      setShowing(true)
       setErrorMessage('Wrong user name or password!')
       setTimeout(() => {
         setErrorMessage(null)
@@ -159,16 +157,19 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogappUser')
     return setUser(null)
   }
+
   return (
     <div className='main_container'>
       <h2>List of blogs collections</h2>
-      <Notification message={errorMessage} textColor={errTextColour} />
+      {showing && (
+        <Notification message={errorMessage} textColor={errTextColour} />
+      )}
 
       {/* == conditional form */}
       {user === null ? (
         loginForm()
       ) : (
-        <div>
+        <>
           <p>{user.name} logged-in</p>{' '}
           <button type='button' onClick={signOff}>
             {' '}
@@ -176,10 +177,23 @@ const App = () => {
           </button>
           <h3> Title Author</h3>
           {blogForm()}
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
-        </div>
+          <table className='dml_table' cellPadding={0} cellSpacing={0}>
+            <thead className='sticky-thc'>
+              <tr>
+                <td>Seq.</td>
+                <td>Title</td>
+                <td>Author</td>
+                <td>URL</td>
+                <td>Likes</td>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((blog, i) => (
+                <Blog key={blog.id} blog={blog} ind={i} />
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   )
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,18 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    <p>
+const Blog = ({ blog, ind }) => (
+  <>
+    <tr>
+      <td>{ind + 1}</td>
+      <td>{blog.title}</td>
+      <td>{blog.author}</td>
+      <td>{blog.url}</td>
+      <td>{blog.likes}</td>
+    </tr>
+
+    {/* <p>
       {blog.title} {blog.author}{' '}
-    </p>
-  </div>
+    </p> */}
+  </>
 )
 
 export default Blog
