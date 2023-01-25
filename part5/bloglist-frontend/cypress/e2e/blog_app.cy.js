9d93166278771d10250df3b457db8a67aae408ea
Amutha Muhunthan <tamamutha@gmail.com>
2023-01-26 10:00:38 +1100

    add more test for fail credentials including css

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,4 @@
-describe('The Home Page', () => {
+describe('Blog app', () => {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     const user = {
@@ -15,43 +15,51 @@ describe('The Home Page', () => {
   })
   // test loggin success 5:18
   describe('Login', () => {
+    it('fails with wrong credentials', () => {
+      cy.contains('Log In').click()
+      cy.get('#username').type('AshaaM')
+      cy.get('#password').type('passAshaaa')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong user name or password!')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      // ! border-style, border-radiusand padding, will pass in Chrome or Electron, but fail in Firefox:
+
+      cy.get('html').should('not.contain', 'Ashaa Muhun')
+    })
+
     it('succed with correct credentials', () => {
       cy.contains('Log In').click()
       cy.get('#username').type('AshaaM')
       cy.get('#password').type('passAshaa')
       cy.get('#login-button').click()
       cy.contains('logged-in')
-    })
-
-    it('fails with wrong credentials', () => {
-      cy.contains('Log In').click()
-      cy.get('#username').type('AshaaM')
-      cy.get('#password').type('passAshaaa')
-      cy.get('#login-button').click()
-      // cy.contains('logged-in')
+      cy.contains('Ashaa logged-in')
     })
   })
 
   // describe('When logged in', () => {
   //   beforeEach(function () {
-  //     cy.contains('Log In').click()
-  //     //  get the content
-  //     cy.get('#username').type('Ash')
-  //     cy.get('#password').type('passAshaa')
-  //     cy.get('#login-button').click()
-  //     cy.contains('logged-in')
+
   //   })
   //   // check content exist
   //   it('a new blog can be created', () => {
   //     cy.contains('Create new blog list').click()
-  //     cy.get('#title').type('a blog created by cypress')
-  //     cy.get('#author').type('Ashaa')
-  //     cy.get('#url').type('https://docs.cypress.io/guides/overview/why-cypress')
+  //     cy.get('#title').type('End to end testing')
+  //     cy.get('#author').type('Matti Luukkainen')
+  //     cy.get('#url').type(
+  //       'https://fullstackopen.com/en/part5/end_to_end_testing/#some-things-to-note'
+  //     )
 
   //     cy.contains('Save').click()
-  //     cy.contains('a blog created by cypress')
-  //     cy.contains('Ashaa')
-  //     cy.contains('https://docs.cypress.io/guides/overview/why-cypress')
+  //     cy.contains('End to end testing')
+  //     cy.contains('Matti Luukkainen')
+  //     cy.contains(
+  //       'https://fullstackopen.com/en/part5/end_to_end_testing/#some-things-to-note'
+  //     )
   //   })
   // })
 })
