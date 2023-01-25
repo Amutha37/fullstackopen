20e0d43ab9a8d98ead2ed58d01fe8918c21165e0
Amutha Muhunthan <tamamutha@gmail.com>
2023-01-25 15:39:51 +1100

    complete exercise 5.17 cyress login form shown

--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -1,8 +1,8 @@
 /* eslint-env node */
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
+    'browser': true,
+    'es6': true,
     'jest/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
@@ -13,13 +13,13 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
-    indent: ['error', 2],
+    'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    eqeqeq: 'error',
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
