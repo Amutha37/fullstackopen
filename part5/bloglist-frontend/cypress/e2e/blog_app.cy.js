87fea11a7bc7c02112e6e045de06abf454e129ed
Amutha Muhunthan <tamamutha@gmail.com>
2023-01-26 16:59:16 +1100

    create 2 blogs after log in once

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -60,42 +60,67 @@ describe('Blog app', () => {
         'https://fullstackopen.com/en/part5/end_to_end_testing/#some-things-to-note'
       )
     })
+
+    it('2 another new blog can be created', () => {
+      cy.contains('Create new blog list').click()
+      cy.get('#title').type('Cypress Testing')
+      cy.get('#author').type('Cypress')
+      cy.get('#url').type('https://docs.cypress.io/guides/overview/why-cypress')
+      cy.contains('Save').click()
+
+      cy.contains('Cypress Testing')
+    })
   })
 
   // create new note
-  describe('create few blogs using request', () => {
-    beforeEach(() => {
-      cy.createBlog({
-        title: 'Cypress Testing',
-        author: 'Cypress',
-        url: 'https://docs.cypress.io/guides/overview/why-cypress',
-      })
+  // describe('when logged in', function () {
+  //   beforeEach(function () {
+  //     cy.login({ username: 'AshaaM', password: 'passAshaa' })
+  //   })
+  //   it('2 another new blog can be created', () => {
+  //     cy.contains('Create new blog list').click()
+  //     cy.get('#title').type('Cypress Testing')
+  //     cy.get('#author').type('Cypress')
+  //     cy.get('#url').type('https://docs.cypress.io/guides/overview/why-cypress')
+  //     cy.contains('Save').click()
 
-      // cy.createBlog({
-      //   title: 'MongoDB',
-      //   author: 'MongoDb',
-      //   url: 'https://account.mongodb.com',
-      // })
+  //     cy.contains('Cypress Testing')
+  //   })
 
-      // cy.createBlog({
-      //   title: 'AWS',
-      //   author: 'Amazon',
-      //   url: 'https://aws.amazon.com/',
-      // })
-    })
+  // })
+  // describe('create new note', function () {
+  //   beforeEach(function () {
+  //     cy.request('POST', 'http://localhost:3003/api/blogs', {
+  //       title: 'Cypress Testing',
+  //       author: 'Cypress',
+  //       url: 'https://docs.cypress.io/guides/overview/why-cypress',
+  //       headers: {
+  //         Authorization: `bearer ${
+  //           JSON.parse(localStorage.getItem('loggedNoteappUser')).token
+  //         }`,
+  //       },
+  //     }).then((response) => {
+  //       localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+  //       cy.visit('http://localhost:3000')
+  //     })
+  //   })
+  // })
+  // describe('create few blogs using request', () => {
+  //   beforeEach(() => {
+  //     cy.createBlog({
+  //       title: 'Cypress Testing',
+  //       author: 'Cypress',
+  //       url: 'https://docs.cypress.io/guides/overview/why-cypress',
+  //     })
+  //   })
 
-    it('Cypress Testing', function () {
-      cy.contains('MongoDB').parent().find('More...').click()
-      cy.get('.count').click()
-    })
-    // change its values
-    // check content exist
-    // it('can like a blog', () => {
-    //   cy.contains('Cypress Testing')
-    //   cy.contains('More...').click()
-    //   cy.contains('+Likes').Click()
-    // })
-  })
+  //   it('add likes count', () => {
+  //     cy.contains('Cypress Testing')
+  //     cy.contains('More...').click()
+  //     // cy.contains('.count').click()
+  //   })
+  //   // change its values
+  // })
 
   // check to its values
 })
