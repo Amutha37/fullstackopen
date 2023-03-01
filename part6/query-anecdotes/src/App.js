8c72509cb1c9c04f1f1c244b0fc8dfb7a88ecff9
Amutha Muhunthan <tamamutha@gmail.com>
2023-03-01 15:54:01 +1100

    add initial fetch all data

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,38 +1,37 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-const App = () => {
+import { useQuery } from 'react-query'
+import { getAll } from './requests'
 
+const App = () => {
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log('vote', anecdote)
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('anecdotes', getAll, { refetchOnWindowFocus: false })
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
