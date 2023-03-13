5e863af2ca004e3514567d4e3c60f556246d96a5
Amutha Muhunthan <67087939+Amutha37@users.noreply.github.com>
2023-03-13 11:19:53 +1100

    Delete part7/router-anecdotes directory
    delete

--- part7/router-anecdotes/src/components/Anecdote.js
+++
@@ -1,8 +0,0 @@
-const Anecdote = ({ anecdote }) => {
-  return (
-    <div>
-      <strong>{anecdote.content}</strong>
-    </div>
-  )
-}
-export default Anecdote
--- part7/router-anecdotes/src/components/Notification.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Notification = ({ notification }) => {
-  return <div className='notification'>{notification}</div>
-}
-
-export default Notification
